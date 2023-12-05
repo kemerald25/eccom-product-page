d061b43eb717a0b6e41c00d4d9c522361c080888
Jujujulex <ishiwuji@gmail.com>
2023-12-05 13:37:40 +0100

    cart page

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,19 @@ import React, { Component } from "react";
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase py-5">
+            <h1 className="display-3">404</h1>
+            <h2>error</h2>
+            <h3>page not found</h3>
+            <h3>
+              the requested URL{" "}
+              <span className="text-danger">{window.location.pathname}</span>
+              was not found
+            </h3>
+          </div>
+        </div>
       </div>
     );
   }
