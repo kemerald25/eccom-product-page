5bf8473e6306a9aab35cc31daa5885a95caea226
Dev Royale <unityekeoba18@gmail.com>
2024-01-06 15:22:12 +0100

    updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,80 +1,60 @@
-// Product.js
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { ProductConsumer } from '../Context';
-import PropTypes from 'prop-types';
-import ProductList from './ProductList';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../Context";
+import PropTypes from "prop-types";
 
-const Product = ({ product }) => {
-  // State to store products fetched from MongoDB
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from MongoDB when the component mounts
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://localhost:5000/api/products'); // Replace with your actual API endpoint
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []); // Run this effect only once when the component mounts
-
-  return (
-    <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-
-      <div className="card">
-        <ProductConsumer>
-          {(value) => (
-            <div
-              className="img-container p-5"
-              onClick={() => value.handleDetail(product.id)}
-            >
-              <Link to="/details">
-                <img src={product.img} alt="img" className="card-img-top" />
-              </Link>
-
-              <button
-                className="cart-btn"
-                disabled={value.inCart(product.id) ? true : false}
-                onClick={() => {
-                  value.addToCart(product.id);
-                  value.openModal(product.id);
-                }}
+export default class Product extends Component {
+  render() {
+    const { id, title, img, price } = this.props.product;
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <ProductConsumer>
+            {(value) => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail(id)}
               >
-                {value.inCart(product.id) ? (
-                  <p className="text-capitalize mb-0" disabled>
-                    {' '}
-                    in cart
-                  </p>
-                ) : (
-                  <i className="fas fa-cart-plus" />
-                )}
-              </button>
-            </div>
-          )}
-        </ProductConsumer>
-        {/* card footer */}
-        <div className="card-footer d-flex justify-content-between">
-          <p className="align-self-center mb-0">{product.title}</p>
+                <Link to="/details">
+                  <img src={img} alt="img" className="card-img-top" />
+                </Link>
 
-          <h5 className="text-blue font-italic mb-0">
-            <span className="me-1">$</span>
-            {product.price}
-          </h5>
+                <button
+                  className="cart-btn"
+                  disabled={value.inCart(id) ? true : false}
+                  onClick={() => {
+                    value.addToCart(id);
+                    value.openModal(id);
+                  }}
+                >
+                  {value.inCart(id) ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      {" "}
+                      in cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
+          {/* card fooer */}
+
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+
+            <h5 className="text-blue font-italic mb-0">
+              <span className="me-1">$</span>
+              {price}
+            </h5>
+          </div>
         </div>
-      </div>
-      {/* Link to AddProduct page */}
-      <ProductList />
-      <Link to="/add-product">Go to AddProduct</Link>
-    </ProductWrapper>
-  );
-};
+      </ProductWrapper>
+    );
+  }
+}
 
 Product.propTypes = {
   product: PropTypes.shape({
@@ -145,5 +125,3 @@ const ProductWrapper = styled.div`
     cursor: pointer;
   }
 `;
-
-export default Product;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+
 import { ProductConsumer } from "../Context";
 
-class ProductList extends Component {
-  state = {
-    products: [], // Initialize an empty array for products
-  };
-
-  // Function to update the products in the state
-  updateProducts = (products) => {
-    this.setState({ products });
-  };
+export default class ProductList extends Component {
   render() {
     return (
       <React.Fragment>
@@ -22,17 +15,17 @@ class ProductList extends Component {
             <div className="row">
               <ProductConsumer>
                 {(value) => {
-                  return value.products.map((product) => (
-                    <Product key={product.id} product={product} />
-                  ));
+                  return value.products.map((product) => {
+                    return <Product key={product.id} product={product} />;
+                  });
                 }}
               </ProductConsumer>
             </div>
           </div>
         </div>
       </React.Fragment>
+
+      // <Product />
     );
   }
 }
-
-export default ProductList;
